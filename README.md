5d93faa89893eebc0f02aa692a4348d26c025a58
Arpad Borsos <arpad.borsos@googlemail.com>
2014-01-21 13:31:44 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Model-array
+
+Array properties for Swatinem/model
+
+[![Build Status](https://travis-ci.org/Swatinem/model-array.png?branch=master)](https://travis-ci.org/Swatinem/model-array)
+[![Coverage Status](https://coveralls.io/repos/Swatinem/model-array/badge.png?branch=master)](https://coveralls.io/r/Swatinem/model-array)
+[![Dependency Status](https://gemnasium.com/Swatinem/model-array.png)](https://gemnasium.com/Swatinem/model-array)
+
+## Installation
+
+    $ component install Swatinem/model-array
+
+## .array(name, [options])
+
+Registers an array property with `name`.
+By default, it emits simle `change` events when any change to the array is made.
+**note**: This is not working and pending some upstream changes
+to [MatthewMueller/array](https://github.com/MatthewMueller/array).
+
+If `options.events` is set to `false`, no change events will be emitted when
+the array changes.
+If `options.detailedEvents` is set, more detailed events are emitted such as:
+
+```js
+('add', element, index)
+('remove', element, index)
+('sort')
+('reverse')
+```
+
+If `options.childEvents` is set, the array is assumed to contain Models itself.
+Events of the child models are emitted like this:
+
+```js
+('child', element, childProperty, value, previousValue)
+```
+
+**note**: The array instance is fixed and can not be overwritten. There is a
+transparent setter that replaces all the elements but the array itself will
+*not* be overwritten.
+
+## Usage
+
+```js
+var Model = require('model');
+var MArray = require('model-array');
+
+var User = new Model(['first', 'last'])
+	.use(MArray)
+	.array('pets');
+
+var instance = new User({first: 'Arpad', last: 'Borsos', pets: ['Ansa']});
+
+instance.on('change pets', function () {});
+instance.pets.push('Zwara'); // triggers change event
+
+```
+
+## License
+
+  LGPLv3
+
---
+++ test/model-array.js
@@ -0,0 +1,89 @@
+/* vim: set shiftwidth=2 tabstop=2 noexpandtab textwidth=80 wrap : */
+"use strict";
+
+var Model = require('model');
+var array = require('../');
+
+describe('array', function () {
+	it('should create a new static `array`', function () {
+		var SomeModel = new Model(['prop'])
+			.use(array);
+		SomeModel.array.should.be.type('function');
+	});
+	it('should allow creating a new array property', function () {
+		var SomeModel = new Model(['prop'])
+			.use(array)
+			.array('arr');
+		var obj = new SomeModel();
+		obj.arr.length.should.eql(0);
+		obj.arr.push(1);
+		obj.arr.length.should.eql(1);
+		obj.arr[0].should.eql(1);
+	});
+	it('should support disabling change events', function () {
+		var SomeModel = new Model(['prop'])
+			.use(array)
+			.array('arr', {events: false});
+		var obj = new SomeModel();
+		obj.arr.length.should.eql(0);
+		obj.on('change', function (name) {
+			throw new Error('unreached');
+		});
+		obj.arr.push('foo');
+	});
+	// TODO: depends on a new upstream array release
+	it.skip('should support change events', function () {
+		var SomeModel = new Model(['prop'])
+			.use(array)
+			.array('arr');
+		var obj = new SomeModel();
+		obj.arr.length.should.eql(0);
+		var called = false;
+		obj.on('change', function (name) {
+			name.should.eql('arr');
+			called = true;
+		});
+		obj.arr.push('foo');
+		called.should.be.true;
+	});
+	it('should support precise change events', function () {
+		var SomeModel = new Model(['prop'])
+			.use(array)
+			.array('arr', {detailedEvents: true});
+		var obj = new SomeModel({arr: ['foo']});
+		obj.arr.length.should.eql(1);
+		SomeModel.on('change', function (instance, prop, event, elem, index) {
+			instance.should.eql(obj);
+			prop.should.eql('arr');
+			event.should.eql('add');
+			elem.should.eql('bar');
+			index.should.eql(1);
+		});
+		obj.arr.push('bar');
+	});
+	it('should support change events for children', function () {
+		var SomeModel = new Model()
+			.use(array)
+			.array('arr', {childEvents: true});
+		var SomeOtherModel = new Model(['prop']);
+
+		var obj = new SomeModel();
+		var sub = new SomeOtherModel();
+		sub.prop = 1;
+		obj.arr.push(sub);
+		obj.once('change arr', function (type, child, prop, val, prev) {
+			type.should.eql('child');
+			child.should.eql(sub);
+			prop.should.eql('prop');
+			val.should.eql(2);
+			prev.should.eql(1);
+		});
+		sub.prop = 2;
+		obj.arr.pop();
+		obj.once('change arr', function () {
+			throw new Error('unreached');
+		});
+		sub.prop = 3;
+	});
+});
+
